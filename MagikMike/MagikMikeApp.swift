d4c3e6a08919ed674ffefb1e08c952185d275615
arunraman <arunraman.19@gmail.com>
2025-09-25 22:07:16 -0700

    Initial Commit

---
+++ MagikMike/MagikMikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagikMikeApp.swift
+//  MagikMike
+//
+//  Created by Arun Raman on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MagikMikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
